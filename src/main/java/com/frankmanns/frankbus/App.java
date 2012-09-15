c5812ae923de76ae2e0a519c6ebfdc6fc80f690e
Benjamin Manns <benmanns@gmail.com>
2012-09-15 17:28:40 -0400

    Add bus poll to App#main.

--- src/main/java/com/frankmanns/frankbus/App.java
+++ src/main/java/com/frankmanns/frankbus/App.java
@@ -1,13 +1,39 @@
 package com.frankmanns.frankbus;
 
-/**
- * Hello world!
- *
- */
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        String url = "http://avlweb.charlottesville.org/RTT/Public/RoutePositionET.aspx?PlatformNo=11983";
+
+        Document document;
+        try {
+            document = Jsoup.connect(url).get();
+        }
+        catch (IOException e) {
+            return;
+        }
+        Elements rows = document.select(".tableET tbody tr:gt(0)");
+
+        for(Element row : rows) {
+            Elements data = row.select("td");
+            String route = data.get(0).text();
+            String destination = data.get(1).text();
+            Integer eta;
+            try {
+                eta = Integer.parseInt(data.get(2).text());;
+            }
+            catch (NumberFormatException e) {
+                continue;
+            }
+            System.out.println(String.format("Route: %s; Destination: %s; ETA: %d", route, destination, eta));
+        }
     }
 }
